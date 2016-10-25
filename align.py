85b92eeada5abaa728676e8faf95aa10d06f96a9
Ben Sandler <ben1sandler@gmail.com>
2016-10-25 03:35:20 -0400

    align.py now outputs a csv with number of times each segment occurrs with each feature instance

--- align.py
+++ align.py
@@ -2,10 +2,12 @@ import argparse
 import csv
 import json
 import collections
+import copy
+import operator
 
 
-def get_word_to_features(wfile):
-    with open(wfile, 'rb') as f:
+def get_word_to_features(feature_file):
+    with open(feature_file, 'rb') as f:
         reader = csv.reader(f)
 
         first_row = next(reader)
@@ -27,8 +29,70 @@ def get_word_to_features(wfile):
     return word_to_features
 
 
+def get_word_to_segments(segment_file):
+    with open(segment_file, 'r') as f:
+        # word -> word segments
+        word_to_segments = {}
+        for line in f:
+            line_split_tabs = line.split('\t')
+            word = line_split_tabs[0]
+            segments = line_split_tabs[1].split()
+            word_to_segments[word] = segments
+    return word_to_segments
+
+
+def get_segment_feature_counts(word_to_features, word_to_segments):
+    # segment -> feature instance (as string like feature: instance) -> count
+    segment_feature_counts = collections.defaultdict(
+        lambda: collections.defaultdict(int))
+
+    for word in word_to_features:
+        if word in word_to_segments:  # TODO: Be more smart about this.
+            segments_for_word = word_to_segments[word]
+            for segment in segments_for_word:
+                for feature_mapping in word_to_features[word]:
+                    for feature_name in feature_mapping:
+                        for feature_instance in feature_mapping[feature_name]:
+                            feature_and_instance = '{}: {}'.format(
+                                feature_name, feature_instance)
+                            segment_feature_counts[segment][
+                                feature_and_instance] += 1
+
+    return segment_feature_counts
+
+
+def write_segment_feature_counts(segment_feature_counts):
+    segment_feature_counts = copy.deepcopy(segment_feature_counts)
+
+    with open('out.csv', 'w') as csvfile:
+        fieldnames_set = set()
+        rows = []  # list of dictionaries. each dictionary is one segment.
+
+        for segment, row in segment_feature_counts.iteritems():
+            for fieldname in row:
+                fieldnames_set.add(fieldname)
+
+            row['Segment'] = segment
+            rows.append(row)
+
+        rows.sort(key=operator.itemgetter('Segment'))
+
+        fieldnames = ['Segment']
+        fieldnames.extend(sorted(list(fieldnames_set)))
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TODO')
-    parser.add_argument('--wfile')
+    parser.add_argument('--feature_file')
+    parser.add_argument('--segment_file')
     args = parser.parse_args()
-    print json.dumps(get_word_to_features(args.wfile))
+
+    word_to_features = get_word_to_features(args.feature_file)
+    word_to_segments = get_word_to_segments(args.segment_file)
+    segment_feature_counts = get_segment_feature_counts(word_to_features,
+                                                        word_to_segments)
+    write_segment_feature_counts(segment_feature_counts)
+    print segment_feature_counts['ed']
